3b998ca57fbd349a594bce445cd8071399631508
tnqkr3494 <tnqkr3494@naver.com>
2023-04-29 18:03:40 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  console.log("i run all the time");
+  const iRunOnlyOnce = () => {
+    console.log("i run only once");
+  };
+  useEffect(iRunOnlyOnce, []); //state변화에도 불구하고 오직 한번만 렌더링되고 리렌더링 안되게 만드는 코드
+  useEffect(() => {
+    if (keyword !== "") {
+      console.log("Search for", keyword);
+    }
+  }, [keyword]); //keyword가 변화할때 리렌더링가능
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Continue"} />
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
