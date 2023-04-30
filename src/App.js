9cc7b5d6463d8efb173cc340a2f0e87635877104
tnqkr3494 <tnqkr3494@naver.com>
2023-04-30 16:59:03 +0900

    Todo

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
-function Hello() {
-  useEffect(function () {
-    console.log("hi :)");
-    return function () {
-      console.log("bye :(");
-    };
-  }, []);
-  useEffect(() => {
-    console.log("hi :)");
-    return () => console.log("bye :(");
-  }, []);
-  return <h1>Hello</h1>;
-}
-// component를 생성할 때 console.log, 파괴할때 return console.log
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]); //...array : array의 요소들을 새로운 배열에 넣어주는것
+    setToDo("");
+  };
+  const deleteBtn = (index) => {
+    setToDos(toDos.filter((item, toDoindex) => index !== toDoindex));
+  };
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>
+            {item}
+            <button onClick={() => deleteBtn(index)}>❌</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
-
+/*   
+<button onClick={deleteBtn(index)}>❌</button>
+이렇게 사용하면 deleteBtn함수가아니라 deleteBtn(index) return된 결과값으로 나와서 onClick이벤트에
+함수만 적어야하는데 값을 적은꼴이 되어버려서 위 주석없는 코드처럼 사용한것.
+*/
 export default App;
