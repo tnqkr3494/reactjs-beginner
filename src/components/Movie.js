9a251a5da7a64abba6cc1e106308da50f871bf26
tnqkr3494 <tnqkr3494@naver.com>
2023-05-01 20:00:49 +0900

    Fin

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,12 +1,12 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-function Movie({ coverImg, title, summary, genres }) {
+function Movie({ id, coverImg, title, summary, genres }) {
   return (
     <div>
       <img src={coverImg} alt={title} />
       <h2>
-        <Link to="/movie">{title}</Link>
+        <Link to={`/movie/${id}`}>{title}</Link>
       </h2>
       <p>{summary}</p>
       <ul>
@@ -17,8 +17,10 @@ function Movie({ coverImg, title, summary, genres }) {
     </div>
   );
 }
+//Link to : a대신 사용함으로 a를 사용하면 발생하는 페이지 재실행(새로고침)문제를 해결해준다.
 
 Movie.propTypes = {
+  id: PropTypes.number.isRequired,
   coverImg: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
